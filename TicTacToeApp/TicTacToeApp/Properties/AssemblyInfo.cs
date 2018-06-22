32ca5780acffdeb14354f76276e3139779b4f00d
johnboy007 <john@game-power.co.uk>
2018-06-22 15:36:28 +0100

    TickTacToe basic tests

---
+++ TicTacToeApp/TicTacToeApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TicTacToeApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TicTacToeApp")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("249fbfdd-5863-4901-8072-565bc0244207")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TicTacToeApp/TicTacToeApp/UnitTest1.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+
+namespace TicTacToeApp
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        /// <summary>
+        ///     a game is over when all fields in a row are taken by a player
+        ///     players take turns taking fields until the game is over
+        ///     a game is over when all fields in a diagonal are taken by a player
+        ///     game is over when all fields are taken
+        ///     there are two players in the game(X and O)
+        ///     a game has nine fields in a 3x3 grid
+        ///     a game is over when all fields in a column are taken by a player
+        ///     a player can take a field if not already taken
+        /// 
+        ///     outputs
+        ///     x wins, o wins, draw
+        /// </summary>
+        [Test]
+        public void TestMethod1()
+        {
+            var tickTackToe = new tickTacToe();
+            tickTackToe.OIsWinner = true;
+            Assert.AreEqual(true, tickTackToe.OIsWinner);
+        }
+        [Test]
+        public void TestMethod2()
+        {
+            var tickTackToe = new tickTacToe();
+            tickTackToe.XIsWinner = true;
+            Assert.AreEqual(true, tickTackToe.XIsWinner);
+        }
+        [Test]
+        public void TestMethod3()
+        {
+            var tickTackToe = new tickTacToe();
+            tickTackToe.IsDraw = true;
+            Assert.AreEqual(true, tickTackToe.IsDraw);
+        }
+        [Test]
+        public void TestMethod4()
+        {
+            var tickTackToe = new tickTacToe();
+            tickTackToe.IsDiagonalWin = true;
+            Assert.AreEqual(true, tickTackToe.IsDiagonalWin);
+        }
+
+        [Test]
+        public void TestMethod5()
+        {
+            var tickTackToe = new tickTacToe();
+            tickTackToe.IsRowWin = true;
+            Assert.AreEqual(true, tickTackToe.IsRowWin);
+        }
+
+        [Test]
+        public void TestMethod6()
+        {
+            var tickTackToe = new tickTacToe();
+            tickTackToe.IsColWin = true;
+            Assert.AreEqual(true, tickTackToe.IsColWin);
+        }
+    }
+
+    internal class tickTacToe
+    {
+        public bool IsRowWin { get; set; }
+        public bool IsColWin { get; set; }
+        public bool IsDiagonalWin { get; set; }
+
+        public bool XIsWinner { get; set; }
+
+        public  bool OIsWinner { get; set; }
+        public bool IsDraw { get;  set; }
+    }
+}
